a05e5c0cf23fb230e2280e20bf11718d2c854426
JangoMandapat <jangomandapat@gmail.com>
2018-07-02 00:32:23 +0800

    User Input added for computestastistics

--- Grades/Grades/Program.cs
+++ Grades/Grades/Program.cs
@@ -11,9 +11,17 @@ namespace Grades
         static void Main(string[] args)
         {
             GradeBook book = new GradeBook();
-            book.AddGrade(91);
-            book.AddGrade(89.5f);
-            book.AddGrade(75);
+            Console.WriteLine("Insert Grades");
+            Console.WriteLine("Insert first grade:");
+            float grade1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Insert Second grade:");
+            float grade2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Insert Third grade:");
+            float grade3 = float.Parse(Console.ReadLine());
+
+            book.AddGrade(grade1);
+            book.AddGrade(grade2);
+            book.AddGrade(grade3);
 
             GradeStatistics stats = book.ComputeStatistics();
             Console.WriteLine("Average Grade is :");
