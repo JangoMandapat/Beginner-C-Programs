669567eb69ed3db0f890aa876fd62a465b175235
JangoMandapat <jangomandapat@gmail.com>
2018-07-01 18:05:31 +0800

    Basic Programs
    Basic Programs

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+         {
+
+            Console.WriteLine("Your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get Last night?");
+            int hoursOfSleep =int.Parse (Console.ReadLine());
+
+
+            Console.WriteLine("Hello," + name);
+            if (hoursOfSleep > 8)
+            {
+                Console.WriteLine("You are well Rested");
+            }
+            else
+            {
+                Console.WriteLine("You need more Sleep");
+            }
+            
+        }
+
+    }
+}
